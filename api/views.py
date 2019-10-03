3a18594dd694b64ce09c29774f1b9829d32359e0
Pablo Endara <lito@Pablos-MBP-2.home>
2019-10-02 21:29:12 -0400

    finished API

--- api/views.py
+++ api/views.py
@@ -3,15 +3,22 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 from .models import Movie, Rating
-from .serializers import MovieSerializers, RatingSerializers
+from .serializers import MovieSerializers, RatingSerializers, UserSerializers
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated )
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -44,7 +51,19 @@ class MovieViewSet(viewsets.ModelViewSet):
              response = {'message': 'You need to provide stars'}
              return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
+
+
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
