f32f1491326babbfa9017a02e4425e9063dd4577
Pablo Endara <lito@Pablos-MBP-2.home>
2019-10-02 20:08:13 -0400

    what auth token recently added

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializers, RatingSerializers
 # Create your views here.
@@ -9,6 +11,7 @@ from .serializers import MovieSerializers, RatingSerializers
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
+    authentication_classes = (TokenAuthentication, )
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -17,7 +20,23 @@ class MovieViewSet(viewsets.ModelViewSet):
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
             user = request.user
-            print("user", user)
+         
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+
+                serializer = RatingSerializers(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializers(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
 
             response = {'message': 'its working'}
             return Response(response, status=status.HTTP_200_OK)
@@ -28,3 +47,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
+    authentication_classes = (TokenAuthentication, )
